e34f5dab0890dcb2386778f25791c96f7845205a
Clare Lee <chihhantw@gmail.com>
2020-08-10 22:38:36 +0800

    update start.end

--- guess-num.py
+++ guess-num.py
@@ -1,10 +1,15 @@
 import random
 
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0 #計算次數
+
 while True:
 	count += 1
-	num = input('從1至100的範圍內，請猜數字:')
+	num = input('請猜數字:')
 	num = int(num)
 	if num == r:
 		print('你猜中了!!!')
